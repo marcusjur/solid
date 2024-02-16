124a14aa35975d9ce4296a1556f1de1a78433e86
MarkK <118257222+marcusjur@users.noreply.github.com>
2024-02-16 12:52:40 +0300

    Update Main.java

--- homework/Main.java
+++ homework/Main.java
@@ -1,9 +1,10 @@
 package homework;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         User user = new User("Bob");
         user.report();
-        user.save();
+        Saveable persister = new Persister(user);
+        persister.save();
     }
 }
