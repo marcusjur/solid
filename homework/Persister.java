1efedcbd48f0f2a356524ecb7f00daa9355a68fa
MarkK <118257222+marcusjur@users.noreply.github.com>
2024-02-16 12:52:13 +0300

    Update Persister.java

--- homework/Persister.java
+++ homework/Persister.java
@@ -1,13 +1,14 @@
 package homework;
 
-public class Persister{
+public class Persister implements Saveable {
     private final User user;
 
-    public Persister(User user){
+    public Persister(User user) {
         this.user = user;
     }
 
-    public void save(){
+    @Override
+    public void save() {
         System.out.println("Save user: " + user.getName());
     }
 }
