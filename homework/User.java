568ebcfa85821cdfcb80fa37dbff9e731ba7f1ae
MarkK <118257222+marcusjur@users.noreply.github.com>
2024-02-16 12:52:29 +0300

    Update User.java

--- homework/User.java
+++ homework/User.java
@@ -1,22 +1,17 @@
 package homework;
 
-public class User{
+public class User {
     private final String name;
 
-    public User(String name){
+    public User(String name) {
         this.name = name;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void save(){
-        Persister persister = new Persister(this);
-        persister.save();
-    }
-
-    public void report(){
+    public void report() {
         System.out.println("Report for user: " + name);
     }
 }
